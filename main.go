35c98b787b90b24d1e819d1f70def22ec421c31c
Carolina Martinez <carolina.martinez@mercadolibre.cl>
2021-01-05 18:39:18 -0300

    declaring variables with primitive data types

--- main.go
+++ main.go
@@ -5,5 +5,29 @@ import (
 )
 
 func main() {
+
 	fmt.Println("Hello from module, Gophers")
+
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	//implicit initialization
+	firstName := "Arthur"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	//(3+4i)
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	//3 4
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
+
 }
