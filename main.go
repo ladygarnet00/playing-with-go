ca1c6766b7224b199d79fc613f369935602699d4
Carolina Martinez <carolina.martinez@mercadolibre.cl>
2021-01-11 20:28:53 -0300

    functions, arguments, parameters, returning values

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/ladygarnet00/playing-with-go/models"
@@ -14,4 +15,62 @@ func main() {
 	}
 
 	fmt.Println(u)
+
+	//Functions, arguments, parameters, returning data
+
+	startWebServer()
+
+	port := 3000
+	retries := 3
+	startWebServerWithPortRetries(port, retries)
+
+	isStarted := startWebServerReturn(port)
+	fmt.Println("Is server started?", isStarted)
+
+	err := startWebServerOnError(port)
+	fmt.Println(err)
+
+	port, err2 := startWebServerMultipleReturn(port)
+	fmt.Println(err2, port)
+
+	//if i dont need the first return value
+	_, err3 := startWebServerMultipleReturn(port)
+	fmt.Println(err3)
+
+}
+
+func startWebServer() {
+	fmt.Println("Starting server...")
+	//do important things
+	fmt.Println("Server started")
+}
+
+func startWebServerWithPortRetries(port, numberRetries int) {
+	fmt.Println("Starting server at port", port, numberRetries)
+	//do important things
+	fmt.Println("Server started")
+}
+
+func startWebServerReturn(port int) bool {
+	fmt.Println("Starting server at port", port)
+	//do important things
+	fmt.Println("Server started")
+
+	return true
+}
+
+func startWebServerMultipleReturn(port int) (int, error) {
+	fmt.Println("An error has ocurred", port)
+	//do important things
+	fmt.Println("Server shut")
+
+	return port, nil
+}
+
+func startWebServerOnError(port int) error {
+	fmt.Println("An error has ocurred", port)
+	//do important things
+	fmt.Println("Server shut")
+
+	return errors.New("An error has ocurred")
 }
